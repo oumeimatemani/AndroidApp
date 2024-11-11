6efb21297c6f06bd2419894d0ea167be1b4186f5
oumeima <oumeima.temani@esprit.tn>
2024-11-12 00:02:46 +0100

    new commit

--- app/src/main/java/com/example/mobile/database/EasyRideDatabase.java
+++ app/src/main/java/com/example/mobile/database/EasyRideDatabase.java
@@ -9,9 +9,5 @@ import com.example.mobile.DAO.UserDao;
 @Database(entities = {UserEntity.class, }, version = 4 )
 public abstract class EasyRideDatabase extends RoomDatabase {
     public abstract UserDao userDao();
-    //public abstract PlanFoodDao planFoodDao();
-    // public abstract ServiceDao serviceDao();
-    // public abstract FoodDao foodDao();
-    // public abstract AnimalDao animalDao();
-    //public abstract AppointmentDao appointmentDao();
+
 }
